d91d1753db6314e2a4cbf4584d5bc49e4975d747
Victor Manuel Cataño <vicmancb@gmail.com>
2015-04-23 11:06:00 -0500

    dist version

--- dist/angular-wizard.js
+++ dist/angular-wizard.js
@@ -1,6 +1,6 @@
 /**
  * Easy to use Wizard library for AngularJS
- * @version v0.4.2 - 2015-04-03 * @link https://github.com/mgonto/angular-wizard
+ * @version v0.4.3 - 2015-04-23 * @link https://github.com/mgonto/angular-wizard
  * @author Martin Gontovnikas <martin@gon.to>
  * @license MIT License, http://www.opensource.org/licenses/MIT
  */
@@ -18,7 +18,7 @@ angular.module("wizard.html", []).run(["$templateCache", function($templateCache
     "    <div class=\"steps\" ng-transclude></div>\n" +
     "    <ul class=\"steps-indicator steps-{{steps.length}}\" ng-if=\"!hideIndicators\">\n" +
     "      <li ng-class=\"{default: !step.completed && !step.selected, current: step.selected && !step.completed, done: step.completed && !step.selected, editing: step.selected && step.completed}\" ng-repeat=\"step in steps\">\n" +
-    "        <a ng-click=\"goTo(step)\">{{step.title || step.wzTitle}}</a>\n" +
+    "        <a>{{step.title || step.wzTitle}}</a>\n" +
     "      </li>\n" +
     "    </ul>\n" +
     "</div>\n" +
